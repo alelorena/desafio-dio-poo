ffce10c33db31a60cd2edb210b34b2d9f4ea58ed
Alexandre Lorena <alexandre.lorena@gmail.com>
2023-04-24 12:37:11 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Mentoria;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descricao curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descricao curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("decricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println("Curso 1: " + curso1);
+        System.out.println("Curso 2: " + curso2);
+        System.out.println("Mentoria: " + mentoria);
+
     }
 }
